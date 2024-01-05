6b7cdc898417862bb2b2e587250d143c4f6536cd
Gautamhirawat <113746508+Gautamhirawat@users.noreply.github.com>
2024-01-06 02:56:36 +0530

    Update urls.py

--- NPS-SECURE-SYSTEM-ishika/devnev_nps/nps/urls.py
+++ NPS-SECURE-SYSTEM-ishika/devnev_nps/nps/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('thank_you/', TemplateView.as_view(template_name='thank_you.html'), name='thank_you'),
+    path('save_survey/', views.save_survey, name='save_survey'),  # Add this line
+
 ]
