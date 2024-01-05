c942da2facb05fcdc5e397aeef330ab5c76698e6
Gautamhirawat <113746508+Gautamhirawat@users.noreply.github.com>
2024-01-05 20:16:37 +0530

    Update urls.py

--- NPS-SECURE-SYSTEM-ishika/devnev_nps/devnev_nps/urls.py
+++ NPS-SECURE-SYSTEM-ishika/devnev_nps/devnev_nps/urls.py
@@ -18,5 +18,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('nps.urls')),     """adding this line so that it can include url.py that is in nps directory"""
+    path('', include('nps.urls')),     
 ]
