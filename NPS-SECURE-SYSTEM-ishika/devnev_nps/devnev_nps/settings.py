6ceba9a49baf9190f87fd603c5c570e5de472431
Gautamhirawat <113746508+Gautamhirawat@users.noreply.github.com>
2024-01-08 12:24:33 +0530

    Update settings.py

--- NPS-SECURE-SYSTEM-ishika/devnev_nps/devnev_nps/settings.py
+++ NPS-SECURE-SYSTEM-ishika/devnev_nps/devnev_nps/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'devnev_nps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'USER': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'PASSWORD': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
 
 # settings.py
 
