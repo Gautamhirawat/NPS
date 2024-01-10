4cc7e7c80c59d2c8be8223eb68c52e6fb40185b4
Gautamhirawat <113746508+Gautamhirawat@users.noreply.github.com>
2024-01-10 13:33:10 +0530

    Update forms.py

---
+++ NPS-SECURE-SYSTEM-ishika/devnev_nps/nps/forms.py
@@ -0,0 +1,13 @@
+# nps/forms.py
+
+from django import forms
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    confirm_password = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
